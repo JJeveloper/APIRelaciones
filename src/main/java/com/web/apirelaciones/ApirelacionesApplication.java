2af08214a284ff2e0aab4f84432c8bfe7bf97bf6
Jarood <jaroodjimili@gmail.com>
2025-03-31 18:32:59 -0500

    iniciando proyecto

---
+++ src/main/java/com/web/apirelaciones/ApirelacionesApplication.java
@@ -0,0 +1,13 @@
+package com.web.apirelaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirelacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirelacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/apirelaciones/ApirelacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.apirelaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirelacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
